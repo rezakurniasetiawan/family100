53ccf840962733226ae7880b9c05d4aed7907e92
rezakurniasetiawan <rezakurniasetiawan@gmail.com>
2025-03-07 22:41:30 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,8 +38,15 @@ class DatabaseSeeder extends Seeder
             ['question' => 'What is the capital of Nigeria?', 'answer' => 'Abuja', 'points' => 10],
             ['question' => 'What is the capital of Ghana?', 'answer' => 'Accra', 'points' => 20],
             ['question' => 'What is the capital of South Africa?', 'answer' => 'Pretoria', 'points' => 30],
-            ['question' => 'What is the capital of Kenya?', 'answer' => 'Nairobi', 'points' => 40]
+            ['question' => 'What is the capital of Kenya?', 'answer' => 'Nairobi', 'points' => 40],
+            ['question' => 'What is the capital of Egypt?', 'answer' => 'Cairo', 'points' => 50],
+            ['question' => 'What is the capital of Canada?', 'answer' => 'Ottawa', 'points' => 60],
+            ['question' => 'What is the capital of Brazil?', 'answer' => 'Brasilia', 'points' => 70],
+            ['question' => 'What is the capital of Japan?', 'answer' => 'Tokyo', 'points' => 80],
+            ['question' => 'What is the capital of France?', 'answer' => 'Paris', 'points' => 90],
+            ['question' => 'What is the capital of Australia?', 'answer' => 'Canberra', 'points' => 100],
         ];
+        
 
         foreach ($questions as $question) {
             \App\Models\Question::create($question);
