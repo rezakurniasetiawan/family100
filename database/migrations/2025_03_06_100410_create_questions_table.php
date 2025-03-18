742601f24afb9abb6fcc4a241d59c69291245bb8
rezakurniasetiawan <56224328+rezakurniasetiawan@users.noreply.github.com>
2025-03-18 21:17:56 +0700

    update

--- database/migrations/2025_03_06_100410_create_questions_table.php
+++ database/migrations/2025_03_06_100410_create_questions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->text('question');
-            $table->foreignId('segment_id')->nullable()->constrained('segments');
+            $table->foreignId('segment_id')->nullable()->constrained('segments')->onDelete('cascade');
             $table->timestamps();
         });
     }
